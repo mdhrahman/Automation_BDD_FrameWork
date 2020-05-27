7bf34fcb8daf01147f94439d9561b5a33453d338
Md Rahman <mrahman@C02YT9HSLVCF.local>
2020-05-26 23:28:23 -0400

    AUtomation FRamework

--- src/test/java/com/ui/qa/helper/BaseTest.java
+++ src/test/java/com/ui/qa/helper/BaseTest.java
@@ -5,14 +5,14 @@ import com.ui.qa.selenium_utils.*;
 public class BaseTest extends BaseClass{
 	protected ValidateLocator validateLocatorObj = new ValidateLocator();
 	protected NavigateMethods navigationObj = new NavigateMethods(driver);
-	protected AssertionValidation assertionObj = new AssertionValidation(driver);
+//	protected AssertionValidation assertionObj = new AssertionValidation(driver);
 	protected ClickElements clickObj = new ClickElements(driver);
 	protected InputMethods inputObj = new InputMethods(driver);
 	protected WaitAndSleepTime waitTimeObj = new WaitAndSleepTime(driver);
 	protected SelectElementByType selectTypeObj = new SelectElementByType();
-	protected GetElements getElementsObj = new GetElements(driver);
-	protected GetValues getValuesobj = new GetValues(driver);
-	protected JSExecutor jsExecutor = new JSExecutor(driver);
-	protected DriverManager driverManage = new DriverManager(driver);
+//	protected GetElements getElementsObj = new GetElements(driver);
+//	protected GetValues getValuesobj = new GetValues(driver);
+//	protected JSExecutor jsExecutor = new JSExecutor(driver);
+//	protected DriverManager driverManage = new DriverManager(driver);
 
 }
